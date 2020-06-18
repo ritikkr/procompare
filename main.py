ab74d29f38e57cdea0ad93f621b14f20d6f4255c
ritikkr <43352981+ritikkr@users.noreply.github.com>
2020-06-18 19:50:02 +0530

    commit

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 if __name__=="__main__":
-  print("Hey ! hihihihi")
-  print("this is my commit from VSC")
+  product_name=input("Enter the name of the product: ")
+  try:
+    search(product_name)
+  except:
+    #purpose of this block is to check if there is any error while searching the product on browser
+    pass
+def search(product_name):
+  print("Search for a product")
