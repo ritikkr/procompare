c81c8c16fa3d30056687ae0275ba8d52577adf5b
ritikkr <43352981+ritikkr@users.noreply.github.com>
2020-06-18 19:51:52 +0530

    search added

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 if __name__=="__main__":
   product_name=input("Enter the name of the product: ")
-  try:
-    search(product_name)
-  except:
-    #purpose of this block is to check if there is any error while searching the product on browser
-    pass
+  search(product_name)
+  
 def search(product_name):
   print("Search for a product")
