60ac2498d353911b522b6407bfdf4eaf19572c38
Ritik Kumar <ritikkumar2405199@gmail.com>
2020-06-19 21:13:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-if __name__=="__main__":
-  product_name=input("Enter the name of the product: ")
-  search(product_name)
-  
-def search(product_name):
-  print("Search for a product")
+"""Compare 'product' price in just a few clicks
+   from more than 0(now) shopping sites."""
+
+
+import argparse
+import amazon
+
+
+def main():
+    """Driver code for 'procompare' app"""
+
+    parser = argparse.ArgumentParser(description="Check Amazon's stock for product")
+    parser.add_argument("product", help="name of the product")
+
+    args = parser.parse_args()
+
+    print(amazon.in_stock(args.product))
+
+
+if __name__ == "__main__":
+    main()
