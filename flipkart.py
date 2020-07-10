b08737425edc45cd1d510d343e0cc787d4d63c27
ritikkr <43352981+ritikkr@users.noreply.github.com>
2020-07-10 19:05:54 +0530

    fixed bugs

--- flipkart.py
+++ flipkart.py
@@ -1,56 +1,48 @@
 """Function related to Flipkart website for scraping data."""
 
-from urllib.parse import quote
 import re
 import requests
 from bs4 import BeautifulSoup
 from product import Product
 
 
-def __get_product_details(products, only_one):
-    product_details_array = []
+def _product_from_product_tag(product_tag):
+    # layout    product   search  name_tag        price_tag   rating_tag
+    # murga     _3liAhj   dreamer a _2cLu-l div   _1vC4OE     div hGSR34
+    # suit      _1SSAGr   suit    a _2mylT6 div   _1vC4OE
+    # mobile    _3O0U0u   mobile  div _3wU53n div _1vC4OE     div hGSR34
 
-    for product in products:
-        # layout    product   search  name_tag        price_tag   rating_tag
-        # murga     _3liAhj   dreamer a _2cLu-l div   _1vC4OE     div hGSR34
-        # suit      _1SSAGr   suit    a _2mylT6 div   _1vC4OE
-        # mobile    _3O0U0u   mobile  div _3wU53n div _1vC4OE     div hGSR34
+    name_tag = product_tag.find(
+        re.compile("(a|div)"), {"class": re.compile("(_2cLu-l|_2mylT6|_3wU53n)")}
+    )
+    price_tag = product_tag.find("div", {"class": "_1vC4OE"})
+    rating_tag = product_tag.find("div", {"class": "hGSR34"})
 
-        product_name_tag = product.find(
-            re.compile("(a|div)"), {"class": re.compile("(_2cLu-l|_2mylT6|_3wU53n)")}
-        )
-        product_name = product_name_tag.text if product_name_tag else ""
+    name = name_tag.text if name_tag else "unknown-product"
+    price = price_tag.text if price_tag else "0.0"
+    rating = rating_tag.text if rating_tag else "0.0"
 
-        product_price_tag = product.find("div", {"class": "_1vC4OE"})
-        product_price = product_price_tag.text if product_price_tag else ""
+    return Product(name, price, rating)
 
-        product_rating_tag = product.find("div", {"class": "hGSR34"})
-        product_rating = product_rating_tag.text if product_rating_tag else "0.0"
 
-        product_details_array.append(
-            Product(product_name, product_price, product_rating)
-        )
+def product_array_from_product_soup(product_soup, only_one):
+    products = []
+
+    for product_tag in product_soup:
+        product = _product_from_product_tag(product_tag)
+        products.append(product)
 
         if only_one:
             break
 
-    return product_details_array
+    return products
 
 
-def __make_products_soup(url):
-    return BeautifulSoup(requests.get(url).text, "html.parser").findAll(
+def make_products_soup(url, headers):
+    return BeautifulSoup(requests.get(url, headers=headers).text, "lxml").findAll(
         "div", {"class": re.compile(("_3liAhj|_1SSAGr|_3O0U0u"))}
     )
 
 
-def __make_url(search_item):
-    return "https://www.flipkart.com/search?q=" + quote(search_item)
-
-
-def get_products(search_item, only_one=False):
-    """Returns an array containing Product class instance."""
-
-    url = __make_url(search_item)
-    soup = __make_products_soup(url)
-
-    return __get_product_details(soup, only_one)
+def make_url(search_item):
+    return "https://www.flipkart.com/search?q=" + "+".join(search_item.split())
