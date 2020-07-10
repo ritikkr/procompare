dfc46c171d5204ee006a63ac5161804c5b0a2eda
ritikkr <43352981+ritikkr@users.noreply.github.com>
2020-07-10 19:06:30 +0530

    new module imported

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 """Compare 'product' price in just a few clicks
-   from more than 0(now) shopping sites."""
-
+   from more than 1 shopping sites."""
 
 import argparse
-import flipkart
-import amazon
 
+import amazon
+import flipkart
+import scrap_helper as sch
 
 def main():
     """Driver code for 'procompare' app"""
 
-    parser = argparse.ArgumentParser(description="compare prices of product from different sites")
+    parser = argparse.ArgumentParser(
+        description="compare prices of product from different sites"
+    )
     parser.add_argument("product", help="name of the product")
 
     args = parser.parse_args()
 
-    for product in flipkart.get_products(args.product):
+    for product in sch.get_products(flipkart, args.product):
         print(product)
 
 
