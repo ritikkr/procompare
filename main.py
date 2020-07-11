3039777d752a0cdcac653a44ba814be1e7a1fd98
ritikkr <43352981+ritikkr@users.noreply.github.com>
2020-07-11 21:50:20 +0530

    module modified

--- main.py
+++ main.py
@@ -2,10 +2,11 @@
    from more than 1 shopping sites."""
 
 import argparse
-
+from tabulate import tabulate
 import amazon
 import flipkart
 import scrap_helper as sch
+
 
 def main():
     """Driver code for 'procompare' app"""
@@ -16,10 +17,23 @@ def main():
     parser.add_argument("product", help="name of the product")
 
     args = parser.parse_args()
-
-    for product in sch.get_products(flipkart, args.product):
-        print(product)
+    print("Flipkart Result")
+    serial_no = 1
+    flipkart_result = sch.get_products(flipkart, args.product)
+    for product in flipkart_result:
+        print("[",serial_no,"] ",product)
+        serial_no +=1
+    print("\n-----------------------------------------------")
+    print("Select a Product (1-",serial_no-1,") : ",end=" ")
+    choice = int(input())
+    print("------------------------------------------------")
+    amazon_result = sch.get_products(amazon, flipkart_result[choice-1].name)
+    amazon_product = [amazon_result[0].name,amazon_result[0].price,amazon_result[0].rating]
+    flipkart_product = [flipkart_result[choice-1].name,flipkart_result[choice-1].price,flipkart_result[choice-1].rating]
+    comparison_result = [amazon_product,flipkart_product]
+    rowIDs =[1,2]
+    print("\n\n\t\t COMPARISON RESULT ")
+    print(tabulate(comparison_result, headers=["Item","Amazon","Flipkart"],showindex=rowIDs,tablefmt="grid"))
     
-
 if __name__ == "__main__":
     main()
