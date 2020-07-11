ba6f0caec41cce9d81491df2e71211b1ad5fc7ee
ritikkr <43352981+ritikkr@users.noreply.github.com>
2020-07-11 21:50:41 +0530

    added link

--- product.py
+++ product.py
@@ -1,10 +1,11 @@
-"""Class to represent product details."""
 
 class Product:
-    def __init__(self, name, price, rating):
+    """Represent the product scraped from websites."""
+    def __init__(self, name, price, rating, link=""):
         self.name = name
         self.price = price
         self.rating = rating
-        
+        self.link = link
+
     def __str__(self):
         return "Name: %s, Price: %s, Rating: %s" % (self.name, self.price, self.rating)
