fee33c6ff934b8be8fafe58a2f49649e7e00e00b
ritikkr <43352981+ritikkr@users.noreply.github.com>
2020-07-12 21:25:46 +0530

    test module modified

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 """Testing script for procompare app."""
 
+import sys
 import flipkart
+import amazon
+import scrap_helper
 
 search_terms = [
     "Kitchen utensils",
@@ -35,21 +38,28 @@ search_terms = [
 
 
 def test_flipkart_get_product():
+    """
+    Testing products on flipkart module
+    """
     for item in search_terms:
-        if flipkart.get_products(item):
-            # product found
-            continue
+        if scrap_helper.get_products(flipkart, item):
+            pass
         else:
             print("flipkart test failed for search term " + item)
-            exit()      
+            sys.exit(0)
+
+
 def test_amazon_get_product():
+    """ 
+    Testing products on amazon module
+    """
     for item in search_terms:
-        if amazon.get.proudcts(item):
-            #product found
-            continue
+        if scrap_helper.get_products(amazon, item):
+            pass
         else:
-            print("Amazon test failed for search term "+item)
-            exit()
+            print("Amazon test failed for search term " + item)
+            sys.exit(0)
+
 
 test_flipkart_get_product()
 test_amazon_get_product()
